d4254802f46b79a3ad3a75c9b754a408cd224208
usuario <usuario@pc21-100>
2023-09-26 17:22:09 +0200

    Solucionado el conflicto crack!

--- src/tutorialGit/HolaMundo.java
+++ src/tutorialGit/HolaMundo.java
@@ -6,17 +6,14 @@ public class HolaMundo {
 		// TODO Auto-generated method stub
 		System.out.println("Hola caracola!");
 		
-<<<<<<< HEAD
 		System.out.println("Bienvenido!");
 		
-=======
 		System.out.println("Bienvenido al curso!");
 		
 		System.out.println("YEsulito 13");
 		
 		System.out.println("Pepe");
 
->>>>>>> branch 'master' of https://github.com/IvanMendozaC097/TutorialDia1.git
 	}
 
 }
